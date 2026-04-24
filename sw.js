85b54396f108da8dc2f3139becf02dc4ca2b26fc
kgrlac-ctrl <kgrlac@gmail.com>
2026-04-24 14:13:33 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,11 +1,3 @@
-self.addEventListener('install', e => {
-  self.skipWaiting();
-  e.waitUntil(caches.open('whatto-v2').then(c => c.addAll([])));
-});
-self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== 'whatto-v2').map(k => caches.delete(k)))));
-});
-self.addEventListener('fetch', e => {
-  // Ne cachiramo API pozive, samo fetchamo network
-  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
-});
+self.addEventListener('install', () => self.skipWaiting());
+self.addEventListener('activate', () => self.clients.claim());
+self.addEventListener('fetch', e => e.respondWith(fetch(e.request)));
