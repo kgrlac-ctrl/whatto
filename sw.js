f088e4ad328e2ec0d6bd462bf8d66663bcd15cc6
kgrlac-ctrl <kgrlac@gmail.com>
2026-04-24 13:51:50 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,2 +1,11 @@
-self.addEventListener('install', e => e.waitUntil(caches.open('whatto-v1').then(c => c.addAll([]))));
-self.addEventListener('fetch', e => e.respondWith(fetch(e.request).catch(() => caches.match(e.request))));
+self.addEventListener('install', e => {
+  self.skipWaiting();
+  e.waitUntil(caches.open('whatto-v2').then(c => c.addAll([])));
+});
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== 'whatto-v2').map(k => caches.delete(k)))));
+});
+self.addEventListener('fetch', e => {
+  // Ne cachiramo API pozive, samo fetchamo network
+  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
+});
